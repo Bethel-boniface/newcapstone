6f16c71db24a0b3728c538f026a9658105182927
bethel <uzoagwabethelboniface@gmail.com>
2026-05-29 06:07:34 +0100

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected');
+
+  socket.on('send_message', (data) => {
+    io.emit('receive_message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
+server.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
